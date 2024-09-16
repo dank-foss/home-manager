0a277fd173203a088b5a93edc190d254a8651dc9
chaddy <chadfoss@duck.com>
2024-09-16 13:15:53 +0530

    make fish directory in home-manager, create custom fish greeter, current generation=24

--- home.nix
+++ home.nix
@@ -30,7 +30,8 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    ".config/fish/config.fish".source = ./config.fish;
+    ".config/fish/config.fish".source = ./fish/config.fish;
+    ".config/fish/functions/fish_greeting.fish".source = ./fish/functions/fish_greeting.fish;
     ".config/nvim/init.lua".source = ./nvim/init.lua;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
