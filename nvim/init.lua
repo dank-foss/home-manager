3a1f28f67cf7edf318bcd7a252607e25306077f7
chaddy <chadfoss@duck.com>
2024-09-17 23:48:04 +0530

    refactor and garbage collect, gen 37

--- nvim/init.lua
+++ nvim/init.lua
@@ -41,7 +41,11 @@ require("lazy").setup({
     end,
     },
 
-    {'nvim-telescope/telescope.nvim', tag = '0.1.8',  dependencies = {'nvim-lua/plenary.nvim'}},
+    {'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = {'nvim-lua/plenary.nvim'}
+    },
+
 },
   -- Configure any other settings here. See the documentation for more details.
   install = { colorscheme = { "habamax" } },   -- colorscheme that will be used when installing plugins.
