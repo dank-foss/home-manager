ca7968c16b33614598043bec96e06a1838fd3499
chaddy <chadfoss@duck.com>
2024-09-18 21:00:53 +0530

    add mason-lspconfig, link mason-lspconfig, 67-68

--- home.nix
+++ home.nix
@@ -46,7 +46,7 @@
     ".config/nvim/lua/plugins/gitsigns.lua".source = ./nvim/lua/plugins/gitsigns.lua;
     ".config/nvim/lua/plugins/comment.lua".source = ./nvim/lua/plugins/comment.lua;
     ".config/nvim/lua/plugins/fugitive.lua".source = ./nvim/lua/plugins/fugitive.lua;
-    ".config/nvim/lua/plugins/mason.lua".source = ./nvim/lua/plugins/mason.lua;
+    ".config/nvim/lua/plugins/lspconf.lua".source = ./nvim/lua/plugins/lspconf.lua;
     
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
