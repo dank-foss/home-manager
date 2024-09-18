e0eccaec640115c60d766fbe8403fb44cfb330b2
chaddy <chadfoss@duck.com>
2024-09-18 21:21:42 +0530

    attempt fix https://www.reddit.com/r/neovim/comments/15jceyb/lspconfig_error_attempt_to_index_local_user/, 72-73

--- nvim/lua/plugins/lspconf.lua
+++ nvim/lua/plugins/lspconf.lua
@@ -18,7 +18,8 @@ return {
     {
 	"neovim/nvim-lspconfig",
         config = function()
-	    require("lspconfig").lua_ls.setup()
+	    local lspconfig = require("lspconfig")
+	    lspconfig.lua_ls.setup({})
 	end
     },
 }
