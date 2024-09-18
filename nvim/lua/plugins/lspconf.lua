d7f3af86e78a2b95e4d5692ae53cffd85c99ba25
chaddy <chadfoss@duck.com>
2024-09-18 21:15:36 +0530

    install nvim-lspconfig, 70-71

--- nvim/lua/plugins/lspconf.lua
+++ nvim/lua/plugins/lspconf.lua
@@ -13,5 +13,13 @@ return {
 		ensure_installed = {"lua_ls"}
 	    })
 	end
-    }
+    },
+
+    {
+	"neovim/nvim-lspconfig",
+	    config = function()
+	    local lspconfig = require("lspconfig")
+	    lspconfig.lua_ls.setup()
+	end
+    },
 }
