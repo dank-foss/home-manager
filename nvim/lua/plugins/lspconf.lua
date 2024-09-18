1fcbedc622f25b1746f9505b7ecd9231a24b78a4
chaddy <chadfoss@duck.com>
2024-09-18 21:04:51 +0530

    fix lsp typo, 68-69

--- nvim/lua/plugins/lspconf.lua
+++ nvim/lua/plugins/lspconf.lua
@@ -8,7 +8,7 @@ return {
     
     {
 	"williamboman/mason-lspconfig.nvim",
-	config = funtion()
+	config = function()
 	    require("mason-lspconfig").setup({
 		ensure_installed = {"lua_ls"}
 	    })
