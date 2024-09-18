084db893179ce9cdc52c58745651b563a688e902
chaddy <chadfoss@duck.com>
2024-09-18 12:00:41 +0530

    add border setting for split, now gen42, pot gen 43

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,6 +6,7 @@ vim.o.clipboard = "unnamedplus"
 vim.o.foldmethod = "manual"
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.laststatus=3 --sets border for splits
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -90,9 +91,13 @@ require("lazy").setup({
 local builtin = require('telescope.builtin')
 vim.keymap.set("n", '<leader>ff', builtin.find_files, {})
 vim.keymap.set("n", '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set("n", '<leader>fb', builtin.buffers, {})
 
 --Neotree
 vim.keymap.set("n", '<leader>fe', ":Neotree filesystem reveal left<CR>", {})
 vim.keymap.set("n", '<leader>fc', ":Neotree close<CR>", {})
 vim.keymap.set("n", '<leader>gs', ":Neotree reveal git_status<CR>", {})
+
+-- Splits
+vim.keymap.set("n", "<leader>vs", ":vs<CR>")
+vim.keymap.set("n", "<leader>hs", ":spl<CR>")
