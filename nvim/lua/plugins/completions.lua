956dbf49c39b7d86f59871930f2b747583ff9408
chaddy <chadfoss@duck.com>
2024-09-21 18:36:41 +0530

    also reformat

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -29,7 +29,6 @@ return {
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 					["<Tab>"] = cmp.mapping.select_next_item(),
-					
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
