99d296899c6e9098c6c55eb7aee39527a310d35a
chaddy <chadfoss@duck.com>
2024-09-21 13:44:30 +0530

    add formatting pretty and linting css-lsp

--- nvim/lua/plugins/nonels.lua
+++ nvim/lua/plugins/nonels.lua
@@ -6,8 +6,9 @@ return {
 	null_ls.setup({
 	    sources = {
 	        null_ls.builtins.formatting.stylua,
-	        -- null_ls.builtins.completion.spell,
-	        -- require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
+	        null_ls.builtins.completion.spell,
+	        null_ls.builtins.diagnostics.css-lsp,
+		null_ls.builtins.formatting.prettier,
 	    },
 	})
 	vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
