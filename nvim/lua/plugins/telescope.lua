0ca5e0dfff9a07faa87627a887acec08760ac27f
chaddy <chadfoss@duck.com>
2024-09-21 18:05:16 +0530

    refactored telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,11 +5,9 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},
 	{
-		{
-			"nvim-telescope/telescope-frecency.nvim",
-			config = function()
-				require("telescope").load_extension("frecency")
-			end,
-		},
+		"nvim-telescope/telescope-frecency.nvim",
+		config = function()
+			require("telescope").load_extension("frecency")
+		end,
 	},
 }
