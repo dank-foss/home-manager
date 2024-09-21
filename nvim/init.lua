95e29c3bebdb75a8ae2145efebd9f9ae05f27b3c
chaddy <chadfoss@duck.com>
2024-09-21 17:20:53 +0530

    add frcecny shortcut

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,7 +6,7 @@ vim.o.clipboard = "unnamedplus"
 vim.o.foldmethod = "manual"
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.laststatus=3 --sets border for splits
+vim.o.laststatus = 3 --sets border for splits
 vim.o.scrolloff = 5
 vim.o.spr = true
 vim.o.tabstop = 4
@@ -15,17 +15,18 @@ vim.o.tabstop = 4
 require("config.lazy")
 
 -- Telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set("n", '<leader>ff', builtin.find_files, {})
-vim.keymap.set("n", '<leader>fg', builtin.live_grep, {})
-vim.keymap.set("n", '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
+vim.keymap.set("n", "<leader>fr", ":Telescope frecency", { desc = "[F]ind [R]ecent" })
 
 --Neotree
-vim.keymap.set("n", '<leader>fe', ":Neotree filesystem reveal left<CR>", {})
-vim.keymap.set("n", '<leader>fd', ":Neotree dir=", {})
-vim.keymap.set("n", '<leader>fc', ":Neotree close<CR>", {})
-vim.keymap.set("n", '<leader>gs', ":Neotree reveal git_status<CR>", {})
+vim.keymap.set("n", "<leader>fe", ":Neotree filesystem reveal left<CR>", {})
+vim.keymap.set("n", "<leader>fd", ":Neotree dir=", {})
+vim.keymap.set("n", "<leader>fc", ":Neotree close<CR>", {})
+vim.keymap.set("n", "<leader>gs", ":Neotree reveal git_status<CR>", {})
 
 -- Splits
 vim.keymap.set("n", "<leader>vs", ":vs<CR>")
