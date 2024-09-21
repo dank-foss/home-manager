011bcbf0597f325174353ab999465a7c1d9d199f
chaddy <chadfoss@duck.com>
2024-09-21 18:33:12 +0530

    add select next item

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -28,6 +28,7 @@ return {
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					["Tab"] = cmp.select_next_item(),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
