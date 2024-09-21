1e07367761b771e309c51c3e45b2679f0d197b06
chaddy <chadfoss@duck.com>
2024-09-21 13:32:02 +0530

    enable auto install treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,12 +3,12 @@ return {
     build = ":TSUpdate",
     config = function () 
 	local configs = require("nvim-treesitter.configs")
-    	
     	configs.setup({
     	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+	auto_install = true,
     	sync_install = false,
     	highlight = { enable = true },
-    	indent = { enable = true },  
+    	indent = { enable = true },
     	})
     end,
 }
