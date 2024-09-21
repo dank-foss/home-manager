5dac381c410bce28e78cf8f34054a6e41bfeb9c1
chaddy <chadfoss@duck.com>
2024-09-21 17:55:25 +0530

    complete completions

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -1,12 +1,16 @@
 return {
 	{
 		"hrsh7th/nvim-cmp",
+
 		dependencies = {
-			'L3MON4D3/LuaSnip',
-			'saadparwaiz1/cmp_luasnip'
-		}
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+
 		config = function()
 			local cmp = require("cmp")
+			require("luasnip.loaders.from_vscode").lazy_load()
 			cmp.setup({
 				snippet = {
 					expand = function(args)
@@ -25,8 +29,7 @@ return {
 					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 				}),
 				sources = cmp.config.sources({
-					-- { name = "nvim_lsp" },
-					-- { name = 'luasnip' }, -- For luasnip users.
+					{ name = "luasnip" }, -- For luasnip users.
 				}, {
 					{ name = "buffer" },
 				}),
