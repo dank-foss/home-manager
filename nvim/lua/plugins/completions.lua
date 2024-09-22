a1066f1347e0a57abe99f052d2f28bb66e94d614
chaddy <chadfoss@duck.com>
2024-09-22 12:06:17 +0530

    add priorities, ad qq

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -33,11 +33,11 @@ return {
 					["<Tab>"] = cmp.mapping.select_next_item(),
 				}),
 				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" }, -- For luasnip users.
+					{ name = "nvim_lsp", priority = 10 },
+					{ name = "luasnip",  priority = 10 }, -- For luasnip users.
 				}, {
+					{ name = "path",  priority = 9 },
 					{ name = "buffer" },
-					{name = "path"}
 				}),
 			})
 		end,
