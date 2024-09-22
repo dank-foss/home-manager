f64aa25096258d5a21bc5f55eabebf27a1542e76
chaddy <chadfoss@duck.com>
2024-09-22 12:41:56 +0530

    cleaning up, going to diff flake lock

--- fish/config.fish
+++
@@ -1,8 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    bind \b backward-kill-word
-    cat ~/.cache/wal/sequences 
-    bind \e\[3\;5~ kill-word
-    zoxide init --cmd "cd" fish | source
-    figlet welcome, GUGA
-end
--- fish/functions/fish_greeting.fish
+++
@@ -1,3 +0,0 @@
-function fish_greeting
-    echo "You are in fish, 'help' for help"
-end
