4d0746a6ef74a81441ae5dbbb45e7a0f9b365354
chaddy <chadfoss@duck.com>
2024-09-22 12:36:57 +0530

    do the splits

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,7 +11,8 @@ vim.o.scrolloff = 5
 vim.o.spr = true
 vim.o.tabstop = 4
 vim.o.wrap = false
-vim.o.ignorecase, vim.o.smartcase = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
 --Lazy
 require("config.lazy")
