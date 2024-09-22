db2efa48f397f4fdfa3ea63d9ff3d9428a0a6f96
chaddy <chadfoss@duck.com>
2024-09-22 12:26:12 +0530

    add diagnostic exception for autosession plugin file

--- nvim/lua/plugins/autosession.lua
+++ nvim/lua/plugins/autosession.lua
@@ -1,12 +1,13 @@
-{
-  'rmagatti/auto-session',
-  lazy = false,
+return {
+	"rmagatti/auto-session",
+	lazy = false,
 
-  ---enables autocomplete for opts
-  ---@module "auto-session"
-  ---@type AutoSession.Config
-  opts = {
-    suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
-    -- log_level = 'debug',
-  }
+	---enables autocomplete for opts
+	---@module "auto-session"
+	---@diagnostic disable-next-line: undefined-doc-name
+	---@type AutoSession.Config
+	opts = {
+		suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+		-- log_level = 'debug',
+	},
 }
