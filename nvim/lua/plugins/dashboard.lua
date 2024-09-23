cf25f889b61d14a24eee728a0fa3d0c7c76ef864
chaddy <chadfoss@duck.com>
2024-09-23 13:11:34 +0530

    disable dashboard

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -1,11 +1,11 @@
 return {
-	'nvimdev/dashboard-nvim',
-	event = 'VimEnter',
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
-	config = true,
-	-- config = function()
-	--   require('dashboard').setup {
-	--     -- config
-	--   }
-	-- end,
+	-- 	'nvimdev/dashboard-nvim',
+	-- 	event = 'VimEnter',
+	-- 	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	-- 	config = true,
+	-- 	-- config = function()
+	-- 	--   require('dashboard').setup {
+	-- 	--     -- config
+	-- 	--   }
+	-- 	-- end,
 }
