aec706d444a6ba6035f9801a9adf7a990aa443ab
chaddy <chadfoss@duck.com>
2024-09-28 12:00:01 +0530

    stop sts and set shiftwidth to 0

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
 -- Custom Options
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
+-- vim.o.softtabstop = 4
+vim.o.shiftwidth = 0
 vim.o.expandtab = false
 vim.o.clipboard = "unnamedplus"
 vim.o.foldmethod = "manual"
