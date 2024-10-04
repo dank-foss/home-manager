2ff854860faf472b01d2f2e766918353f9a74620
chaddy <chadfoss@duck.com>
2024-10-04 18:39:01 +0530

    add hlchunk

--- nvim/lua/plugins/indent.lua
+++ nvim/lua/plugins/indent.lua
@@ -1,4 +1,11 @@
 return {
--- 	"nvimdev/indentmini.nvim",
--- 	config = true,
+	"shellRaining/hlchunk.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("hlchunk").setup({
+			chunk = { enable = true },
+			indent = { enable = true },
+			line_num = { enable = true },
+		})
+	end,
 }
