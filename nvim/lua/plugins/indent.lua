d8490a8f2b8d553531e9aa067c182ec254a0a6e9
chaddy <chadfoss@duck.com>
2024-10-05 11:46:23 +0530

    add defstyle to all three mods

--- nvim/lua/plugins/indent.lua
+++ nvim/lua/plugins/indent.lua
@@ -2,14 +2,14 @@ return {
 	"shellRaining/hlchunk.nvim",
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
-		def_style = {
+		local def_style = {
 			{ fg = "#AB572F" },
 			{ fg = "#D14A18" },
 		}
 		require("hlchunk").setup({
 			chunk = { enable = true, style = def_style },
-			indent = { enable = true },
-			line_num = { enable = true },
+			indent = { enable = true, style = def_style },
+			line_num = { enable = true, style = def_style },
 		})
 	end,
 }
