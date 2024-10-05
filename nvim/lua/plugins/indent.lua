ce4e82fccf28942059d71fd513142058ae31c237
chaddy <chadfoss@duck.com>
2024-10-05 11:35:12 +0530

    change style in indent and add ww

--- nvim/lua/plugins/indent.lua
+++ nvim/lua/plugins/indent.lua
@@ -2,8 +2,12 @@ return {
 	"shellRaining/hlchunk.nvim",
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
+		local def_style = {
+			{ fg = "#79865E" },
+			{ fg = "#AB572F" },
+		}
 		require("hlchunk").setup({
-			chunk = { enable = true },
+			chunk = { enable = true, style = def_style },
 			indent = { enable = true },
 			line_num = { enable = true },
 		})
