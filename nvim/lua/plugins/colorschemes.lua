69449a2568f9d699df8ac06b691a7e7cd377e4fc
chaddy <chadfoss@duck.com>
2024-10-28 21:35:42 +0530

    rm comments from everforest

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -12,11 +12,11 @@ return {
 		version = false,
 		lazy = false,
 		priority = 1000,
-		-- config = function()
-		-- 	require("everforest").setup({
-		-- 		-- Your config here
-		-- 	})
-		-- end,
+		config = function()
+			require("everforest").setup({
+				-- Your config here
+			})
+		end,
 	},
 	{
 		"catppuccin/nvim",
