3650ea5bced72a2741fd9d2d2d7017ba368b0eb5
chaddy <chadfoss@duck.com>
2024-11-07 18:17:26 +0530

    take out return comment from dashboard, cant return nil

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -1,11 +1,11 @@
--- return {
--- 	-- 	'nvimdev/dashboard-nvim',
--- 	-- 	event = 'VimEnter',
--- 	-- 	dependencies = { 'nvim-tree/nvim-web-devicons' },
--- 	-- 	config = true,
--- 	-- 	-- config = function()
--- 	-- 	--   require('dashboard').setup {
--- 	-- 	--     -- config
--- 	-- 	--   }
--- 	-- 	-- end,
--- }
+return {
+	-- 	'nvimdev/dashboard-nvim',
+	-- 	event = 'VimEnter',
+	-- 	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	-- 	config = true,
+	-- 	-- config = function()
+	-- 	--   require('dashboard').setup {
+	-- 	--     -- config
+	-- 	--   }
+	-- 	-- end,
+}
