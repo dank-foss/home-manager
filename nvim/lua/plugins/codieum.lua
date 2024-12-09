9478998b7deaa79c05f470a81cf3e347c85c8fca
chaddy <chadfoss@duck.com>
2024-12-09 14:52:00 +0530

    change codeium accept func from tab to normal l

--- nvim/lua/plugins/codieum.lua
+++ nvim/lua/plugins/codieum.lua
@@ -1,4 +1,10 @@
 return {
 	"Exafunction/codeium.vim",
 	event = "BufEnter",
+	config = function()
+		vim.g.codeium_no_map_tab = 1
+		vim.keymap.set("n", "l", function()
+			return vim.fn["codeium#Accept"]()
+		end, { expr = true, silent = true })
+	end,
 }
