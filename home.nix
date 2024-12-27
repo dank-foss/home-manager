eccbce8e86f824aca62e4a72a89c9aeb34366f55
chaddy <chadfoss@duck.com>
2024-12-27 21:56:42 +0530

    add lua and luarocks

--- home.nix
+++ home.nix
@@ -14,6 +14,8 @@
   home.packages = with pkgs; [
 	zoxide
 	neovim
+	lua
+	luajitPackages.luarocks
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
