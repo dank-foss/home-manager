f4628f26f50e82a1d99215d6d9edb5a11b1e89b8
chaddy <chadfoss@duck.com>
2024-12-29 00:10:31 +0530

    install git

--- home.nix
+++ home.nix
@@ -23,6 +23,7 @@
     luajitPackages.luarocks
     obsidian
     nixd
+    git
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
