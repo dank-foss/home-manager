aabd16e9379a082b78e669766d152fefba21817d
chaddy <chadfoss@duck.com>
2025-01-01 14:38:38 +0530

    format home

--- home.nix
+++ home.nix
@@ -25,7 +25,7 @@
     obsidian
     nixd
     git
-	statix
+    statix
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
