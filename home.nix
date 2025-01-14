e26cec4ecfcf6aa51280b602ad436aeeb8e9bfe0
chaddy <chadfoss@duck.com>
2025-01-14 20:02:57 +0530

    add gitg to packages

--- home.nix
+++ home.nix
@@ -28,6 +28,7 @@
     statix
     brave
     stow
+    gitg
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
