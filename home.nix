dc84f18dbeae02a383c0530d03be3df9d6b2124f
chaddy <chadfoss@duck.com>
2025-01-31 22:13:15 +0530

    in rg

--- home.nix
+++ home.nix
@@ -42,6 +42,7 @@
     figlet
     pfetch-rs
     jetbrains-mono
+    ripgrep
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
