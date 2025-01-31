c1e49a20d3ff5ea89e031db9f6fb7293ea27e07c
chaddy <chadfoss@duck.com>
2025-01-31 22:20:38 +0530

    add git keybinds

--- dots/nvim/.config/nvim/lua/plugins/fugitive.lua
+++ dots/nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -1,3 +1,8 @@
 return {
 	"tpope/vim-fugitive",
+	config = function()
+		vim.keymap.set("n", "<leader>ga", ":Git add .<CR>")
+		vim.keymap.set("n", "<leader>gc", ":Git ac ''")
+		vim.keymap.set("n", "<leader>gmc", ":Git commit -m ''")
+	end,
 }
