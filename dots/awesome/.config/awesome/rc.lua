e492e5ca4e71cd6876da18a085c345c804026567
chaddy <chadfoss@duck.com>
2025-02-01 11:59:30 +0530

    rm excess buttons

--- dots/awesome/.config/awesome/rc.lua
+++ dots/awesome/.config/awesome/rc.lua
@@ -502,22 +502,9 @@ end)
 
 -- Add a titlebar if titlebars_enabled is set to true in the rules.
 client.connect_signal("request::titlebars", function(c)
-	-- buttons for the titlebar
-	local buttons = gears.table.join(
-		awful.button({}, 1, function()
-			c:emit_signal("request::activate", "titlebar", { raise = true })
-			awful.mouse.client.move(c)
-		end),
-		awful.button({}, 3, function()
-			c:emit_signal("request::activate", "titlebar", { raise = true })
-			awful.mouse.client.resize(c)
-		end)
-	)
-
 	awful.titlebar(c):setup({
 		{ -- Left
 			awful.titlebar.widget.iconwidget(c),
-			buttons = buttons,
 			layout = wibox.layout.fixed.horizontal,
 		},
 		{ -- Middle
@@ -525,7 +512,6 @@ client.connect_signal("request::titlebars", function(c)
 				align = "center",
 				widget = awful.titlebar.widget.titlewidget(c),
 			},
-			buttons = buttons,
 			layout = wibox.layout.flex.horizontal,
 		},
 		{ -- Right
