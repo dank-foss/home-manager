8ed2cf8315d597d57ef18179f902e17b96908f9e
chaddy <chadfoss@duck.com>
2025-02-01 08:02:03 +0530

    in gitsigns, change prettierd and markdownlint

--- dots/nvim/.config/nvim/lua/plugins/nonels.lua
+++ dots/nvim/.config/nvim/lua/plugins/nonels.lua
@@ -6,17 +6,18 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.prettierd,
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
 				null_ls.builtins.formatting.nixfmt,
 				null_ls.builtins.formatting.biome,
 
 				null_ls.builtins.diagnostics.fish,
-				null_ls.builtins.diagnostics.markdownlint,
+				null_ls.builtins.diagnostics.markdownlint_cli2,
 				null_ls.builtins.diagnostics.stylelint,
 
 				null_ls.builtins.code_actions.statix,
+				null_ls.builtins.code_actions.gitsigns,
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
