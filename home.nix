7a8f2c02de49e3cabc11432da121c3dfecedbd68
chaddy <chadfoss@duck.com>
2025-02-09 21:07:41 +0530

    in sublime

--- home.nix
+++ home.nix
@@ -54,7 +54,8 @@
     pkg-config
     atuin
     legcord
-	alpaca
+    alpaca
+    sublime-merge
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
