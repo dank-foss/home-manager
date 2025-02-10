c4a62f9a2d12fc459bac43a61e2f6224e7418fc2
chaddy <chadfoss@duck.com>
2025-02-10 09:27:37 +0530

    in fretboard

--- home.nix
+++ home.nix
@@ -56,6 +56,7 @@
     legcord
     alpaca
     sublime-merge
+    fretboard
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
