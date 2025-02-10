fc9a965468642d4ba83ffc3e1ea935473cb682e6
chaddy <chadfoss@duck.com>
2025-02-10 12:53:08 +0530

    format fish conf

--- dots/fish/.config/fish/config.fish
+++ dots/fish/.config/fish/config.fish
@@ -1,11 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     bind \b backward-kill-word
-    cat ~/.cache/wal/sequences 
+    cat ~/.cache/wal/sequences
     bind \e\[3\;5~ kill-word
-    zoxide init --cmd "cd" fish | source
+    zoxide init --cmd cd fish | source
     figlet welcome, GUGA
-	atuin init fish | source
-	abbr -a gu 'gitui' 
-	kdlfmt completions fish
+    atuin init fish | source
+    abbr -a gu gitui
 end
