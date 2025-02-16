0542124f88e0300e86d377f072a6f12f2f530040
chaddy <chadfoss@duck.com>
2025-02-16 08:21:49 +0530

    rebase, in bluetui wev and wlcopy

--- home.nix
+++ home.nix
@@ -57,7 +57,9 @@
     alpaca
     sublime-merge
     fretboard
-	bluetui
+    bluetui
+    wl-clipboard-rs
+    wev
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
