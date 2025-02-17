d163cdd80fe7b9593bebe6e675054b72093491ff
chaddy <chadfoss@duck.com>
2025-02-17 21:35:09 +0530

    in waypaper

--- home.nix
+++ home.nix
@@ -60,6 +60,7 @@
     bluetui
     wl-clipboard-rs
     wev
+    waypaper
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
