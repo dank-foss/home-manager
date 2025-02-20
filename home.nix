929dee4d6e204781638820de87175f5386f44cfd
chaddy <chadfoss@duck.com>
2025-02-20 11:14:15 +0530

    create a let and in block which defines package lists, in the in block stitch together the lists

--- home.nix
+++ home.nix
@@ -1,17 +1,13 @@
-{ config, pkgs, ... }:
-
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "baka";
-  home.homeDirectory = "/home/baka";
-  targets.genericLinux.enable = true;
+  config,
+  pkgs,
+  my-shell,
+  ...
+}:
 
-  home.stateVersion = "24.05"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = with pkgs; [
+let
+  pkg-list = with pkgs; [
+    dart-sass
     gowall
     deno
     nodejs
@@ -74,7 +70,20 @@
     #   echo "Hello, ${config.home.username}!"
     # '')
   ];
+in
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "baka";
+  home.homeDirectory = "/home/baka";
+  targets.genericLinux.enable = true;
 
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+
+  home.packages = pkg-list ++ [ my-shell ];
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
