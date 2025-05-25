59463bfe35a71e8d31d33360692109e139fa815c
chaddy <chadfoss@duck.com>
2025-05-25 11:40:39 +0530

    refactor fish conf

--- dots/fish/.config/fish/config.fish
+++ dots/fish/.config/fish/config.fish
@@ -1,9 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     bind \b backward-kill-word
-    cat ~/.cache/wal/sequences 
+    cat ~/.cache/wal/sequences
     bind \e\[3\;5~ kill-word
-    zoxide init --cmd "cd" fish | source
+    zoxide init --cmd c fish | source
     figlet welcome, GUGA
-	atuin init fish | source
+    abbr -a -- nv nvim
+    atuin init fish | source
 end
