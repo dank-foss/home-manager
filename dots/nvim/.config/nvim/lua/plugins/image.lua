78c910ba7c6eae9233cc1729e94741c93ecb6311
chaddy <chadfoss@duck.com>
2025-10-03 01:00:26 +0530

    add image.nvim

---
+++ dots/nvim/.config/nvim/lua/plugins/image.lua
@@ -0,0 +1,16 @@
+return {
+	"3rd/image.nvim",
+	build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+	opts = {
+		processor = "magick_cli",
+	},
+	config = function()
+		require("image").setup({
+			integrations = {
+				markdown = {
+					download_remote_images = false,
+				},
+			},
+		})
+	end,
+}
